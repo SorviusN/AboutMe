6ca9e4ee8bc3f98a9a28eb530e0ce01df89cec9f
Jona <jbrown6@berklee.edu>
2021-05-05 23:31:43 -0700

    Added extra questions.Cleaned up HTML/CSS.

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ let ageQuestion = prompt('Hello there ' + userName + '! Welcome to my webpage. H
 // Prompts the user for a question
 
 if (ageQuestion === '24') {
-  amountCorrect += 1;
+  amountCorrect++;
   //console.log('That is correct, great guessing!');
   alert('That is correct, great guessing!');
 }
@@ -25,7 +25,7 @@ let birthdayQuestion = prompt('Second question - In what month was I born?');
 birthdayQuestion = birthdayQuestion.toLowerCase();
 
 if (birthdayQuestion === 'dec' || birthdayQuestion === 'december') {
-  amountCorrect += 1;
+  amountCorrect++;
   //console.log('Amazing! You are correct. I was born on December 6th, 1996');
   alert('Amazing! You are correct. I was born on December 6th, 1996');
 }
@@ -40,12 +40,12 @@ sportQuestion = sportQuestion.toLowerCase();
 
 switch(sportQuestion) {
 case 'hockey':
-  amountCorrect += 1;
+  amountCorrect++;
   //console.log('That is correct! I played Hockey for 10 years growing up - both on ice and roller blades. I also played a bit of baseball.');
   alert('That is correct! I played Hockey for 10 years growing up - Both on ice and roller blades. I also played a bit of baseball.');
   break;
 case 'baseball':
-  amountCorrect += 1;
+  amountCorrect++;
   //console.log('I did play some baseball growing up, however hockey was my main sport.');
   alert('I did play some baseball growing up, however hockey was my main sport.');
   break;
@@ -73,7 +73,7 @@ let animalQuestion = prompt('Last question - Can you guess my favorite animal? H
 animalQuestion = animalQuestion.toLowerCase();
 
 if (animalQuestion === 'turtle') {
-  amountCorrect += 1;
+  amountCorrect ++;
   //console.log('Correct. I have owned a turtle for most of my life and enjoy their laid-back lifestyle.');
   alert('Correct. I have owned a turtle for most of my life and enjoy their laid-back lifestyle.');
 }
@@ -82,7 +82,73 @@ else {
   alert('While I probably like ' + animalQuestion + ' as well, my favorite animal is the turtle. I enjoy their resilience and laid-back lifestyle.');
 }
 
-alert('Thank you very much for answering my questions, ' + userName + ', you got ' + amountCorrect + ' out of 5 correct!');
+
+console.log('creating variable named myAnswer for the answer to the numbers question.');
+let myAnswer = 2;
+
+console.log('creating variable named countDown for the amount of guesses left for the user as well as decrementing a counter for the while loop.');
+let countDown = 4;
+
+console.log('pre-initializing num for the while loop. Not entirely sure if necessary.');
+
+for (let i = 0; i < 4; i++) {
+
+  let num = prompt('Try to guess my number between 1 and 10, you have ' + countDown + ' guesses left. Please answer in number format.');
+
+  if (num > myAnswer) {
+    alert('Not quite, try guessing lower.');
+    countDown--;
+    console.log('Decrementing Countdown.');
+  }
+
+  else if (num < myAnswer) {
+    alert('Not quite, try guessing higher.');
+    countDown--;
+    console.log('Decrementing Countdown.');
+  }
+  else {
+    amountCorrect++;
+    break;
+  }
+}
+
+alert('Thanks for guessing, ' + userName +', the number was ' + myAnswer + '!');
+
+console.log('initializing artists');
+let artists = [
+  'prince',
+  'dolly parton',
+  'amy winehouse',
+  'chet atkins',
+  'lady gaga',
+  'freddie mercury',
+  'joni mitchell',
+  'frank sinatra',
+  'jimi hendrix'];
+
+function isAnswer(artistAnswer) {
+  for (let a = 0; a < 10; a++) {
+    if (artistAnswer.toLowerCase() === artists[a]) {
+      console.log('Correct Artist');
+      return true;
+    }
+  }
+  console.log('Artist not found if at the end of the for loop.');
+  alert('Artist not found on my list..try again.');
+  return false;
+}
+
+for (let a = 0; a < 7; a++) {
+  let artistAnswer = prompt('Can you guess an American music artist in my top ten list?');
+  if (isAnswer(artistAnswer) === true) {
+    alert('You found an artist on my list. Congrats!');
+    amountCorrect++;
+    break;
+  }
+}
+
+alert('Thank you very much for answering my questions, ' + userName + ', you got ' + amountCorrect + ' out of 7 correct!');
+
 
 
 
